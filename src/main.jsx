9766a4242c8731eb8e2e13863c1654a555b85e66
malineczky <aleksandra.m.kot@gmail.com>
2023-07-13 15:42:15 +0200

    add fake_api, data, get data from api to jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
+import { createClient } from "@supabase/supabase-js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
