99c477ad429a170c8b106db2bb9f8625fe277062
malineczky <aleksandra.m.kot@gmail.com>
2023-07-14 12:38:59 +0200

    add Gifts

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import { useEffect, useState } from "react";
+import Gifts from "./components/Gifts";
 
 function App() {
-    const [gifts, setGifts] = useState([]);
-
-    useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal;
-
-        getGifts(signal).then((data) => setGifts(data));
-
-        return () => {
-            controller.abort();
-        };
-    }, []);
-
-    async function getGifts(signal) {
-        const response = await fetch("http://localhost:3000/gifts", { signal });
-        return await response.json();
-    }
-
     return (
         <>
-            {gifts.map((gift) => (
-                <div
-                    key={gift.id}
-                    style={{
-                        border: "1px solid black",
-                        padding: "10px",
-                        margin: "10px",
-                    }}
-                >
-                    <h2>{gift.name}</h2>
-                    <p>{gift.description}</p>
-                    <p>PLN {gift.price}</p>
-                </div>
-            ))}
+            <Gifts />
         </>
     );
 }
---
+++ src/components/Gifts.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function Gifts() {
+    const [gifts, setGifts] = useState([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        getGifts(signal).then((data) => setGifts(data));
+
+        return () => {
+            controller.abort();
+        };
+    }, []);
+
+    async function getGifts(signal) {
+        const response = await fetch("http://localhost:3000/gifts", { signal });
+        return await response.json();
+    }
+
+    return (
+        <>
+            {gifts.map((gift) => (
+                <div
+                    key={gift.id}
+                    style={{
+                        border: "1px solid black",
+                        padding: "10px",
+                        margin: "10px",
+                    }}
+                >
+                    <h2>{gift.name}</h2>
+                    <p>{gift.description}</p>
+                    <p>PLN {gift.price}</p>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default Gifts;
