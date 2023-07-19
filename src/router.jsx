04c07723e3d880058bcaf1004b3162f98f795fef
malineczky <aleksandra.m.kot@gmail.com>
2023-07-19 11:47:22 +0200

    add Outlet

--- src/router.jsx
+++ src/router.jsx
@@ -1,25 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import Gift from "./components/Gift";
 import AddGift from "./components/AddGift";
 import EditGift from "./components/EditGift";
+import Layout from "./components/Layout";
+import App from "./App";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
-    },
-    {
-        path: "gift/:giftId",
-        element: <Gift />,
-    },
-    {
-        path: "add-gift",
-        element: <AddGift />,
-    },
-    {
-        path: "edit-gift/:giftId",
-        element: <EditGift />,
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+            },
+            {
+                path: "gift/:giftId",
+                element: <Gift />,
+            },
+            {
+                path: "add-gift",
+                element: <AddGift />,
+            },
+            {
+                path: "edit-gift/:giftId",
+                element: <EditGift />,
+            },
+        ],
     },
 ]);
 
